7b6da9912e55df01122ac39db713900948b1d88b
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2025-03-27 09:46:10 +0530

    added zaprun

--- zapier_hook/src/index.ts
+++ zapier_hook/src/index.ts
@@ -6,12 +6,31 @@ app.use(express.json())
 const prisma = new PrismaClient()
 
 
-app.post('hooks/catch/:userId/:zapId/', (req, res) => {
+app.post('hooks/catch/:userId/:zapId/', async (req, res) => {
     const zapId = req.params.zapId
     const userId = req.params.userId
+    const data = req.body
 
-    prisma.zap.create({
-        data: {
-            triggerId:
+    await prisma.$transaction(async (tx) => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: data,
+            }
         })
 
+        const zapRunOUtBoxResponse = await tx.zapRunOutbox.create({
+            //@ts-ignore
+            data: {
+                zapRunId: run.id,
+            }
+        })
+
+    })
+    res.send('ok')
+
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+}   
